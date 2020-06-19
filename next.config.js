5c70b176321d52aa3889e2ef4a601c16bd553ad5
Michael Trieu <mtrieu@sold.com>
2020-06-19 16:42:39 -0700

    Split config files for running separate environments

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
+const merge = require('lodash.merge');
+
+const CONFIG_ENV = process.env.CONFIG_ENV;
+CONFIG_ENV !== 'test' && console.log('CONFIG_ENV', CONFIG_ENV);
+
 /**
  * Default or shared configuration should go here
  */
-module.exports = {
+let defaultConfig = {
   publicRuntimeConfig: {
     BUILD_VERSION: process.env.BUILD_VERSION,
+    STATIC_CDN_URL: '',
   },
 };
+
+switch (CONFIG_ENV) {
+  case 'dev':
+    config = require('./config/next/dev');
+    break;
+  case 'prod':
+    config = require('./config/next/prod');
+    break;
+
+  default:
+    config = require('./config/next/localhost');
+    break;
+}
+
+module.exports = merge(defaultConfig, config);
